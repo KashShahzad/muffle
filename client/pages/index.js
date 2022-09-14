39df5b128efa6d443a2ebb891a1583591bb44ab1
Kashif Shahzad <r.kashifshahzad@outlook.com>
2022-09-14 22:27:02 +0500

    bigbang

---
+++ client/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Muffle: Order Anything Anywhere</title>
+        <meta name="description" content="food delivery application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      homepage
+    </div>
+  );
+}
